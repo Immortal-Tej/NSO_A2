5b50c45970e0df510cc7692383996f1f627abdfa
Immortal-Tej <vesa22@student.bth.se>
2026-06-12 15:33:38 +0200

    three scripts fully working

--- rev1_ssh_wrapper.sh
+++ rev1_ssh_wrapper.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-exec ssh -i /home/tejeswar/.ssh/nso_key \
-    -o StrictHostKeyChecking=no \
-    -o UserKnownHostsFile=/dev/null \
-    -o "ProxyCommand=ssh -i /home/tejeswar/.ssh/nso_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p ubuntu@194.47.155.186" \
-    "$@"
+TARGET=${@: -1}
+if [[ "$TARGET" == "194.47.155.186" ]]; then
+    exec ssh -i /home/tejeswar/.ssh/nso_key \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        "$@"
+else
+    exec ssh -i /home/tejeswar/.ssh/nso_key \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        -o "ProxyCommand=ssh -i /home/tejeswar/.ssh/nso_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p ubuntu@194.47.155.186" \
+        "$@"
+fi
